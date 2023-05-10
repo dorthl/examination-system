e17e01579e5d172056764cd3a22fe7104e431be0
dorthl <x344527085@outlook.com>
2023-05-10 17:47:36 +0800

    build

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 cd exam_online_ui
-npm i
+npm i 
+npm run build:prod
+rm -fr ../exam-online-api/ruoyi-admin/src/main/resources/static
+cp -r dist ../exam-online-api/ruoyi-admin/src/main/resources/static
 cd ../exam-online-api
 mvn install
+docker build -t registry.cn-hangzhou.aliyuncs.com/dorthl/examination:latest .
+docker push registry.cn-hangzhou.aliyuncs.com/dorthl/examination:latest
